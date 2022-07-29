d39e1a278b56cd02cf784226574c75578ef221f2
jefferson <jefftrabalho1@gmail.com>
2022-07-29 00:56:33 -0300

    primeiro commit sts

---
+++ BsmgenerationApplication.java
@@ -0,0 +1,13 @@
+package org.generation.exemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsmgenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsmgenerationApplication.class, args);
+	}
+
+}
---
+++ ObjetivogenerationApplication.java
@@ -0,0 +1,13 @@
+package org.generation.exemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjetivogenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjetivogenerationApplication.class, args);
+	}
+
+}
